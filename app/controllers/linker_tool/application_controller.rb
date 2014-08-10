8d87b52fa48f3897159a1e38af1e85b96500f33d
andysingularity <AndySingularity@gmail.com>
2014-08-10 20:11:28 +0000

    Version 0.1.0

--- app/controllers/linker_tool/application_controller.rb
+++ app/controllers/linker_tool/application_controller.rb
@@ -2,7 +2,7 @@ module LinkerTool
   class ApplicationController < ActionController::Base
     def main_plate(prms,mn_lnkr_frm)
       @params = prms
-      @main_linker_form = mn_lnkr_frm.inspect
+      @main_linker_form = mn_lnkr_frm
       test_routes = []
   
       Rails.application.routes.routes.each do |route|
