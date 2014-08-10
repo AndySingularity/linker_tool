645ab7ed458b46ba11dac17880b48f4432c4b45f
andysingularity <AndySingularity@gmail.com>
2014-08-10 19:30:30 +0000

    Version 0.0.9

--- app/assets/javascripts/linker_tool/application.js
+++ app/assets/javascripts/linker_tool/application.js
@@ -15,93 +15,89 @@
 //= require_tree .
 
 $(document).ready(function() {
-	var main_form = $("form#main_linker_form");
-	var main_form_div = $("form#main_linker_form div");
-	var edit_form = $("pre#edit_input_form");
+	var main_form = $("div#linker_form_gem form#main_linker_form");
+	var main_form_div = $("div#linker_form_gem form#main_linker_form div");
+	var edit_form = $("div#linker_form_gem pre#edit_input_form");
 	var last_form_html = localStorage.getItem("linker_form_gem_html");
 	if (last_form_html !== "") {
 		main_form.replaceWith(last_form_html);
-		main_form = $("form#main_linker_form");
-		main_form_div = $("form#main_linker_form div");
+		main_form = $("div#linker_form_gem form#main_linker_form");
+		main_form_div = $("div#linker_form_gem form#main_linker_form div");
 	}
-	var authenticity_token = $("input[name='authenticity_token']").clone().wrap('<p>').parent().html();
+	var authenticity_token = $("div#linker_form_gem input[name='authenticity_token']").clone().wrap('<p>').parent().html();
 	var num = 1;
-	$("input[name='authenticity_token']").remove();
+	$("div#linker_form_gem input[name='authenticity_token']").remove();
 	main_form.attr("method","get");
-	$("#link").change(function(){
-			main_form.attr("action",$("#link").val());
+	$("div#linker_form_gem #link").change(function(){
+			main_form.attr("action",$("div#linker_form_gem #link").val());
+			show_full_form_html(false);
 	});
-	$("#mtd").change(function(){
-		if ($("#mtd").val() === "get") { 
-				main_form.attr("method","get");
-				$("input[name='_method']").remove();
-				$("input[name='authenticity_token']").remove();
-		} else {
-				set_post_method($("#mtd").val());
-		}
+	$("div#linker_form_gem #mtd").change(function(){
+		change_method();
+		show_full_form_html(false);
 	});
-	$("input#main_linker_form_submit").click(function(){
+	change_method();
+	$("div#linker_form_gem input#main_linker_form_submit").click(function(){
 		main_form.submit();
 	});
-	$("input#new_input_submit").click(function(){
-		var name = $("input#new_input_name").val();
-		var type = $("select#new_input_type").val();
-		var str = '<span id="input_name">' + name + '</span> ' + '<input id="' + name + '" name="' + name + '" type="' + type + '">' + '<input id="' + num + '" name="edit_input" value="edit" type="button">'+ '<input id="' + num + '" name="delete_input" value="delete" type="button">';
+	$("div#linker_form_gem #new_input_submit").click(function(){
+		check_num();
+		var name = $("div#linker_form_gem input#new_input_name").val();
+		var type = $("div#linker_form_gem select#new_input_type").val();
+		var this_input = '<input id="' + name + '" name="' + name + '" type="' + type + '">'
+		var str = '<span id="input_name">' + name + '</span> ' + this_input + '<input id="' + num + '" name="edit_input" value="edit" type="button">'+ '<input id="' + num + '" name="delete_input" value="delete" type="button">';
 		if (main_form.children().length > 1) {
 			str = '<hr>' + str;
 		}
 		str = '<div id="' + num + '">' + str + "</div";
 		main_form.append(str);
-		num = num + 1;
 		set_edit_delete_listener();
+		show_full_form_html(false);
 	});
 	set_edit_delete_listener();
-	$("input#new_input_clear").click(function(){
-		$("input#new_input_name").val("");
-		$("select#new_input_type").val("text");
+	$("div#linker_form_gem #new_input_clear").click(function(){
+		$("div#linker_form_gem input#new_input_name").val("");
+		$("div#linker_form_gem select#new_input_type").val("text");
 	});
-	$("input#new_input_delete_all").click(function(){
-		var block_html = $("form#main_linker_form div").clone().wrap('<p>').parent().html();
+	$("div#linker_form_gem #new_input_delete_all").click(function(){
+		var block_html = $("div#linker_form_gem form#main_linker_form div").clone().wrap('<p>').parent().html();
 		main_form.empty();
 		main_form.append(block_html);
-		main_form_div = $("form#main_linker_form div");
+		main_form_div = $("div#linker_form_gem form#main_linker_form div");
+		edit_form.hide();
+	  show_full_form_html(false);
 	});
-	$("#full_form_html").click(function(){
-		var txta = $("#full_form_html_textarea");
-		txta.toggle();
-		var need_text = main_form.clone().wrap('<p>').parent().html();
-		txta.val(need_text);
-		var txtaLineHeight = 20;
-		var txtaHeight = txta.get(0).scrollHeight;
-		var numberOfLines = Math.ceil(txtaHeight/txtaLineHeight - 1);
-		txta.css({"height":"","width":"100%"});
-		txta.attr("rows",numberOfLines);
+	$("div#linker_form_gem #full_form_html").click(function(){
+		show_full_form_html(true);
 	});
-	$("#full_routes_button").click(function(){
-		$("#full_routes").toggle();
+	$("div#linker_form_gem #full_routes_button").click(function(){
+		$("div#linker_form_gem #full_routes").toggle();
 	});
-	$("#delete_form_cookie").click(function(){
+	$("div#linker_form_gem #delete_form_cookie").click(function(){
 		localStorage.setItem("linker_form_gem_html","");
 	});
-	$("#save_form_cookie").click(function(){
+	$("div#linker_form_gem #save_form_cookie").click(function(){
 		localStorage.setItem("linker_form_gem_html",main_form.clone().wrap('<p>').parent().html());
 	});
-	$("input[type='text'").change(function(){
+	$("div#linker_form_gem input[type='text'").change(function(){
 		$(this).attr("value",$(this).val());
+	});
+	$("div#linker_form_gem #show_all_linker_components").click(function(){
+		$("div#linker_form_gem #linker_form_gem_main_panel").toggle();
 	});
 	
 	function set_post_method(method_name){
 		main_form.attr("method","post");
 		if (method_name == "post"){
-			$("input[name='_method']").remove();
+			$("div#linker_form_gem input[name='_method']").remove();
 		} else {
-			if (!$("input[name='_method']").length) {
+			if (!$("div#linker_form_gem input[name='_method']").length) {
 					main_form_div.append('<input name="_method" type="hidden" value="' + method_name + '">');
 			} else {
-					$("input[name='_method']").val(method_name);
+					$("div#linker_form_gem input[name='_method']").val(method_name);
 			}
 		}
-		if (!$("input[name='authenticity_token']").length) {
+		if (!$("div#linker_form_gem input[name='authenticity_token']").length) {
 			main_form_div.append(authenticity_token);
 		}
 	}
@@ -131,7 +127,7 @@ $(document).ready(function() {
 	}
 	
 	function find_my(input_id){
-		var el = $("div#" + input_id).children().first().next();
+		var el = $("div#linker_form_gem div#" + input_id).children().first().next();
 		if (el.is("span")) {
 			el = el.next();
 		}
@@ -139,7 +135,7 @@ $(document).ready(function() {
 	}
 	
 	function set_edit_delete_listener(){
-		$("input[name='delete_input'").unbind("click").click(function(){
+		$("div#linker_form_gem input[name='delete_input'").unbind("click").click(function(){
 			if ($(this).attr("id") === main_form.children().first().next().attr("id")) {
 				$(this).parent().next().find("hr").remove();
 			}
@@ -147,8 +143,9 @@ $(document).ready(function() {
 				edit_form.hide();
 			}
 			$(this).parent().remove();
+			show_full_form_html(false);
 		});
-		$("input[name='edit_input'").unbind("click").click(function(){
+		$("div#linker_form_gem input[name='edit_input'").unbind("click").click(function(){
 			if( edit_form.css("display") == "none" ) {
 				edit_form.show();
 				edit_form_method($(this).attr("id"));
@@ -160,18 +157,20 @@ $(document).ready(function() {
 				}
 			}
 			set_update_input_listener();
+			show_full_form_html(false);
 		});
 	}
 	
 	function set_update_input_listener(){
-		$("input#update_input").unbind("click").click(function(){
+		$("div#linker_form_gem input#update_input").unbind("click").click(function(){
 			var el = find_my(edit_form.val());
-			el.attr($("#edit_form_attr").val(),$("#edit_form_val").val());
-			if ($("#edit_form_attr").val() == "name"){
-				el.prev().html($("#edit_form_val").val());
+			el.attr($("div#linker_form_gem #edit_form_attr").val(),$("div#linker_form_gem #edit_form_val").val());
+			if ($("div#linker_form_gem #edit_form_attr").val() == "name"){
+				el.prev().html($("div#linker_form_gem #edit_form_val").val());
 			}
 			edit_form_method(edit_form.val());
 			set_update_input_listener();
+			show_full_form_html(false);
 		});
 	}
 	function writeCookie(name,value,days) {
@@ -199,6 +198,34 @@ $(document).ready(function() {
 		}
 		return '';
 	}
-	localStorage.setItem("key", "value");
-	var value = localStorage.getItem("key");
+	function change_method(){
+		if ($("div#linker_form_gem #mtd").val() === "get") { 
+				main_form.attr("method","get");
+				$("div#linker_form_gem input[name='_method']").remove();
+				$("div#linker_form_gem input[name='authenticity_token']").remove();
+		} else {
+				set_post_method($("div#linker_form_gem #mtd").val());
+		}
+	}
+	function check_num(){
+		if (main_form.children().length > 1) {
+			num = parseInt(main_form.children().last().attr("id")) + 1;
+			console.log(num);
+		}
+	}
+	function show_full_form_html(tgl){
+		var txta = $("div#linker_form_gem #full_form_html_textarea");
+		if (tgl){
+			txta.toggle();
+		}
+		var need_text = main_form.clone().wrap('<p>').parent().html();
+		txta.val(need_text);
+		var txtaLineHeight = 20;
+		var txtaHeight = txta.get(0).scrollHeight;
+		var numberOfLines = Math.ceil(txtaHeight/txtaLineHeight - 1);
+		txta.css({"height":"","width":"100%"});
+		txta.attr("rows",numberOfLines);
+	}
+	// localStorage.setItem("key", "value");
+	// var value = localStorage.getItem("key");
 });
