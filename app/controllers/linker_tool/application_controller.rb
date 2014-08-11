6ba40580e12b983682e039558fb76df291a11b16
andysingularity <AndySingularity@gmail.com>
2014-08-11 06:46:45 +0000

    Version 0.1.0

--- app/controllers/linker_tool/application_controller.rb
+++ app/controllers/linker_tool/application_controller.rb
@@ -3,7 +3,7 @@ module LinkerTool
     def main_plate(prms,mn_lnkr_frm)
       @params = prms
       # --- Here parse @main_linker_form
-      @main_linker_form = mn_lnkr_frm.inspect.gsub!("\\","")
+      @main_linker_form = raw(mn_lnkr_frm)
       # ---
       test_routes = []
   
